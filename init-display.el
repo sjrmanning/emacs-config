c8066b7eb198ac89b2d8a369263d7f7aa900404e
Simon Manning <simon@ecksdee.org>
2012-10-20 23:04:01 +0800

    Added some Windows-specific display changes.

--- init-display.el
+++ init-display.el
@@ -5,10 +5,18 @@
 ;; Solarized colour-theme
 (load-theme 'solarized-dark t)
 
-;; Font
-;; Checks if the font given exists first.
-(defvar font-name "Meslo LG L DZ-12")
+;; Set custom font
+;; Meslo is larger on Windows, so size differs between `system-type'.
+(defvar font-name (if (eq system-type 'windows-nt)
+                      "Meslo LG L DZ-10"
+                      "Meslo LG L DZ-12"))
+
+;; Check that given font exists first.
 (unless (null (x-list-fonts font-name))
   (set-face-font 'default font-name))
+
+;; Disable menu-bar on Windows.
+(when (eq system-type 'windows-nt)
+  (menu-bar-mode -1))
 
 (provide 'init-display)
