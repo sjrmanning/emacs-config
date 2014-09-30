0395021d07faa5d9e1e9a16ef183f0e28d32838d
Simon Manning <simon@ecksdee.org>
2014-09-30 10:24:37 +0800

    Fix /usr/local/bin not being in exec-path early enough (fixes gpg issue).

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 ;; Prepare paths.
 (setq load-path (cons "~/.emacs.d/init"   load-path))
 (setq load-path (cons "~/.emacs.d/lib"    load-path))
+(add-to-list 'exec-path "/usr/local/bin")
 (add-to-list 'custom-theme-load-path "~/.emacs.d/lib/color-themes")
 
 ;; Ensure required packages are installed.
